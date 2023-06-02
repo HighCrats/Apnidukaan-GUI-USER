e4618d6fd8310661e6ec6e1c6d825f8923edbb91
HighCrats <131246898+HighCrats@users.noreply.github.com>
2023-06-02 15:04:20 +0530

    Update Web-Api.js

--- src/api/Web-Api.js
+++ src/api/Web-Api.js
@@ -1,18 +1,18 @@
 
 const apiPoint = {
-  PRODUCT_LIST: "https://backend-ffna.onrender.com/product/recent-product",
-  PRODUCT_DATA: "https://backend-ffna.onrender.com/product/list",
-  CATEGORY_LIST: "https://backend-ffna.onrender.com/category/list",
-  USER_CONTACT: "https://backend-ffna.onrender.com/contactUser/contact",
-  USER_SIGNIN: "https://backend-ffna.onrender.com/user/signin",
-  USER_SIGNUP: "https://backend-ffna.onrender.com/user/signup",
-  SELLER_POST: 'https://backend-ffna.onrender.com/sell/sellproduct',
-  SOLD_POST: 'https://backend-ffna.onrender.com/sold/soldproduct',
-  SELLER_PRODUCT: "https://backend-ffna.onrender.com/sell/fetch-sell",
-  PRODUCT_BY_CATEGORY: "https://backend-ffna.onrender.com/product/searchCategory",
-  USER_CHECK:'https://backend-ffna.onrender.com/user/checkUser',
-  FORGOTT_PASSWORD: 'https://backend-ffna.onrender.com/user/updatePassword',
-  USER_SMS:'https://backend-ffna.onrender.com/smsUser/sms'
+  PRODUCT_LIST: "/https://backend-ffna.onrender.com/product/recent-product",
+  PRODUCT_DATA: "/https://backend-ffna.onrender.com/product/list",
+  CATEGORY_LIST: "/https://backend-ffna.onrender.com/category/list",
+  USER_CONTACT: "/https://backend-ffna.onrender.com/contactUser/contact",
+  USER_SIGNIN: "/https://backend-ffna.onrender.com/user/signin",
+  USER_SIGNUP: "/https://backend-ffna.onrender.com/user/signup",
+  SELLER_POST: '/https://backend-ffna.onrender.com/sell/sellproduct',
+  SOLD_POST: '/https://backend-ffna.onrender.com/sold/soldproduct',
+  SELLER_PRODUCT: "/https://backend-ffna.onrender.com/sell/fetch-sell",
+  PRODUCT_BY_CATEGORY: "/https://backend-ffna.onrender.com/product/searchCategory",
+  USER_CHECK:'/https://backend-ffna.onrender.com/user/checkUser',
+  FORGOTT_PASSWORD: '/https://backend-ffna.onrender.com/user/updatePassword',
+  USER_SMS:'/https://backend-ffna.onrender.com/smsUser/sms'
 };
 
 
